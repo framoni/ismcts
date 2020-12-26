674841894efd2e85f61bd4bfdddd6b271cda730d
Francesco Ramoni <francesco.ramoni@finleap.com>
2020-12-26 16:15:51 +0100

    Implement two-side damage calculation

--- pokemon.py
+++ pokemon.py
@@ -27,7 +27,7 @@ class Pokemon:
 
     def __init__(self, name, item, evs, nature, trait, moves, gen=5):
         self.name = name
-        self.health = 1
+        self.health = None
         self.status = {}
         self.boosts = {}
         self.item = item
@@ -65,6 +65,8 @@ class GameStatus:
         ]  # sum of expected rewards
 
     def ended(self):
+        if any([p.health == None for p in self.team1 + self.team2]):
+            return False
         if any([p.health > 0 for p in self.team1]) and any([p.health > 0 for p in self.team2]):
             return False
         return True
@@ -73,20 +75,28 @@ class GameStatus:
         # decide who starts first
         self.team1[self.current1].speed
 
-    def step(self, actions):
-        child = copy.deepcopy(self)
+    def calc_damage(self, action, p1, p2, c):
         p1 = self.team1[self.current1]
         p2 = self.team2[self.current2]
         content = utils.js_string.format(self.gen, p1.name, p1.item, p1.nature,
                                          json.dumps(p1.evs).replace('"', ''), json.dumps(p1.boosts).replace('"', ''),
                                          p2.name, p2.item, p2.nature,
                                          json.dumps(p2.evs).replace('"', ''), json.dumps(p2.boosts).replace('"', ''),
-                                         actions[0])
+                                         action)
         with open("calc.mjs", "w") as f:
             f.write(content)
         out = json.loads(check_output(["node", "calc.mjs"]))
-        damage = random.choice(out["damage"])  # this should be a random node
-        child.team2[child.current2].health = ...
+        damage = random.choice(out["damage"]) if type(out["damage"]) == list else out["damage"]  # this should be a random node
+        if c.health == None:
+            c.health = out['defender']['originalCurHP']
+        old_health = c.health
+        c.health -= damage
+        print("{} health {} --> {}".format(c.name, old_health, c.health))
+
+    def step(self, actions):
+        child = copy.deepcopy(self)
+        self.calc_damage(actions[0], self.team1[self.current1], self.team2[self.current2], child.team2[child.current2])
+        self.calc_damage(actions[1], self.team2[self.current2], self.team1[self.current1], child.team1[child.current1])
         # calculate both damages, modify pokemon stats, return new status
         return child
 
